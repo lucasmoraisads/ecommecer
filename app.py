059b8f0639ec0166d14395ba543be3c25478f6f4
Lucas Morais <118077670+lucasmoraisads@users.noreply.github.com>
2024-07-10 06:46:15 -0500

    add novas rotas
    add base no html

--- app.py
+++ app.py
@@ -3,5 +3,16 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 @app.route('/')
-def ecommerce():
-    return render_template("index.html")
+def page_home():
+    return render_template("home.html")
+
+@app.route('/produtos')
+def page_produtos():
+    itens = [
+        {'id':1, 'nome':'Celular', 'cod_barra':191991, 'preco':1200},
+        {'id':2, 'nome':'Notbook', 'cod_barra':121345, 'preco':2300},
+        {'id':3, 'nome':'teclado', 'cod_barra':191923, 'preco':250},
+        {'id':4, 'nome':'monitor', 'cod_barra':201991, 'preco':600},
+        {'id':5, 'nome':'mause', 'cod_barra':202012, 'preco':130}
+    ]
+    return render_template('produtos.html', itens=itens)
